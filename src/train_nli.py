2182ef137cc9af4cd4981e732a568420e8ac142e
Leandro R.S <le.rsouza@gmail.com>
2022-01-25 18:34:10 -0300

    add transformers optimizers

--- src/train_nli.py
+++ src/train_nli.py
@@ -1,7 +1,14 @@
+from torch.optim import Optimizer
+from transformers import optimization as transformers_optim
+from pytorch_lightning.utilities.cli import OPTIMIZER_REGISTRY
+
 from data_nli import TextClassificationDataModule
 from model_nli import TextClassificationModel
 
 from cli import CustomCLI
+
+
+OPTIMIZER_REGISTRY.register_classes(transformers_optim, Optimizer, override=True)
 
 
 cli = CustomCLI(
